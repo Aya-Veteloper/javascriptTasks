388a1dbcb4df3232ab45235d2bf1e6dd5971c04e
aya-m-98 <106470807+aya-m-98@users.noreply.github.com>
2023-01-14 15:04:30 +0200

    task1

---
+++ task1/task.js
@@ -0,0 +1,44 @@
+
+// First task
+
+// function sum(num1, num2) {
+//     if (typeof num1 == "number" && typeof num2 == "number"){
+//     var result = num1 + num2;
+//     console.log(result);
+//     } else {
+//         alert("enter valid number");
+//     }
+// }
+
+// sum();
+
+
+
+
+
+// Second task
+
+// function triangleArea (base, height) {
+//     var area = (base * height) / 2;
+//     return area;
+// }
+
+
+// function triangleAreaCalc (){
+//     var base = prompt("Enter base of triangle");
+//     var height = prompt("Enter height of triangle")
+//     var result= triangleArea(base, height);
+//     document.write(result);    
+// }
+
+
+
+// Third task
+
+// function giveMeSomething (a){
+//     var myWord = "Something";
+//     console.log(myWord.concat(" ", a));
+// }
+
+// giveMeSomething("hello world");
+
