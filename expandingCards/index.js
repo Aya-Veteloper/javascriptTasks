5d1f22fe65d9c32dbe9b080490e71a495111f9ae
aya-m-98 <106470807+aya-m-98@users.noreply.github.com>
2023-05-18 18:49:50 +0200

    expandingCards
    mini tutorial with Javascript

---
+++ expandingCards/index.js
@@ -0,0 +1,12 @@
+let panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', function(){
+        removeActiveClasses();
+        this.classList.add('active');
+    })
+})
+
+let removeActiveClasses = function removeActiveClasses() {
+    panels.forEach((panel) => panel.classList.remove('active') )
+}
